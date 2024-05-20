7fa6d47c18bc6ad2d7ada98a372dd36dc775b5d6
Mian Salman <skdevelopers@live.com>
2024-05-20 21:54:06 +0500

    Update index.js
    Update index.js to add ondrej/php repository for PHP 8.2

--- src/index.js
+++ src/index.js
@@ -8,6 +8,9 @@ async function run() {
     const iniValues = core.getInput('ini-values').split('\n').map(val => val.trim());
 
     await exec.exec(`sudo apt-get update`);
+    await exec.exec(`sudo apt-get install -y software-properties-common`);
+    await exec.exec(`sudo add-apt-repository -y ppa:ondrej/php`);
+    await exec.exec(`sudo apt-get update`);
     await exec.exec(`sudo apt-get install -y php${phpVersion}`);
 
     for (const extension of extensions) {
